9e98c3a9cb2eb0499720ecd19315d41844062abf
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2021-12-21 15:08:58 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// Current age in human years
+const myAge = 13;
+// A vairiable that has a value of 2
+let earlyYear = 2;
+// The above variable is now multiplied by 10.5
+// It's output is 21
+earlyYear *= 10.5;
+// A varibale that has the value of the current years in human age, reduced by 2
+let laterYears = myAge - 2;
+// The below variable multiplies the above variable by 4, after it's reduced by 2
+laterYears *= 4;
+// This variable will tell us the dog years, 
+let myAgeInDogYears = earlyYear + laterYears;
+// The variable here decides the name of the person
+myName = 'Solomon'.toUpperCase();
+// The text that will be printed on the console that will tell us the human age, converted into dog age.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
